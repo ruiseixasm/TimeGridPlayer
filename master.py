6ba615a55b9b19382577c85e02efe2d4d2445cd8
Rui Monteiro <ruiseixas@yahoo.com>
2023-10-26 12:21:33 +0100

    Staff functions

--- master.py
+++ master.py
@@ -5,46 +5,79 @@ def main():
     print("Hello!")
 
 class Master:
-    def __init__(self, steps, frames, tempo, staff_ids):
+    def __init__(self, steps, frames, tempo):
         self.steps = steps
         self.frames = frames
         self.tempo = tempo
+
         self.timeGrid = []
-        self.eventGrid = []
+        self.staffEvents = []
+        self.staffRulers = []
+
         for i in range(self.steps*self.frames):
 
-            frameData = {'frame_id': 0, 'tag': "", 'step': 0, 'frame': 0, 'time': 0, 'triggered': False}
+            frameData = {'frame_id': 0, 'position': "", 'step': 0, 'frame': 0, 'time': 0, 'triggered': False}
             frameData['frame_id'] = i
             frameData['step'] = int(i/self.frames)
             frameData['frame'] = int(i % self.frames)
-            frameData['tag'] = str(frameData['step']) + "." + str(frameData['frame'])
+            frameData['position'] = str(frameData['step']) + "." + str(frameData['frame'])
             frameData['time'] = self.tempo * frameData['step'] + self.tempo * frameData['frame'] / self.frames
 
             self.timeGrid.append(frameData)
-    
-        self.staff_ids = staff_ids
-        self.staff = []
-
-        for staff_id in self.staff_ids:
-            staffData = {'staff_id': staff_id, 'event': None, 'triggered': False}
 
 
     def play(self):
         # current timestamp in seconds
-        startTime = time.time()
-        print("Timestamp:", startTime)
+        print("Timestamp:", time.time())
         nextFrameID = 0
+        startTime = time.time()
+        lastTime = time.time() - startTime
         while nextFrameID < len(self.timeGrid):
             if (time.time() - startTime > self.timeGrid[nextFrameID]['time']):
-                print(f"{nextFrameID}\t{time.time() - startTime}")
+                actualTime = time.time() - startTime
+                print(f"{nextFrameID}\t{self.timeGrid[nextFrameID]['position']}\t{actualTime:.6f}\t{actualTime - lastTime:.6f}")
+
+                actual_position = [self.timeGrid[nextFrameID]['position']]
+
+                actual_events = [
+                    dictionary for dictionary in self.staffEvents if dictionary['position'] in actual_position
+                ]
+
                 self.timeGrid[nextFrameID]['triggered'] = True
                 nextFrameID += 1
+                lastTime = time.time() - startTime
 
+    def tick(self):
+        pass
+        
+    def addRuler(self, name, position, ruler):
+        newRuler = {
+            'name': name,
+            'position': position,
+            'ruler': ruler,
+            'triggered': False
+        }
+        self.staffRulers.append(newRuler)
+    
+    def listRulers(self, position):
+        pass
+
+    def addEvents(self, name, position, events):
+        newEvents = {
+            'name': name,
+            'position': position,
+            'events': events,
+            'triggered': False
+        }
+        self.staffEvents.append(newEvents)
+
+    def listEvents(self, position):
+        pass
                 
     def __str__(self):
         finalString = ""
         for frame in self.timeGrid:
-            finalString += f"{frame['frame_id']}\t{frame['tag']}\t{frame['time']}\n"
+            finalString += f"{frame['frame_id']}\t{frame['position']}\t{frame['time']}\n"
         return finalString
 
 
--- user.py
+++ user.py
@@ -1,5 +1,16 @@
 from master import Master
 
-master = Master(4, 4, 3.123, ['c', 'c#', 'd', 'd#', 'e', 'f'])
+master = Master(4, 4, 3.123)
+
+master.addRuler("first", "1.1", ['c', 'c#', 'd', 'd#', 'e', 'f'])
+
+print_hello = lambda rule : print("Hello ", rule)
+
+master.addEvents("first", "2.1", [
+        None,
+        print_hello, None,
+        lambda rule : print("Hi ", rule),
+        None, None
+    ])
 print(master)
 master.play()
