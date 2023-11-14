1bad3e18e2a7a1f128df7dc7d28d6b95e99de55d
Rui Monteiro <ruiseixas@yahoo.com>
2023-11-14 16:57:28 +0000

    User argument parameters redefined as note and key

--- user_player_4.py
+++ user_player_4.py
@@ -25,21 +25,24 @@ note = Piano.Note("notes", midi_synth, 440, 1, 4, 1, play_range=[[0, 0], [1, 0]]
 
 print("\n\n")
 
-trigger.rulers().add({'type': "actions", 'group': "triggers", 'position': [1, 1], 'lines': [trigger]})
-trigger.rulers().add({'type': "actions", 'group': "triggers", 'position': [3, 1], 'lines': [trigger]})
+trigger.rulers().add({'type': "actions", 'group': "trigger", 'position': [1, 1], 'lines': [trigger]})
+trigger.rulers().add({'type': "actions", 'group': "trigger", 'position': [3, 1], 'lines': [trigger]})
 trigger.rulers().filter(type="actions").sort().print().print_lines()
 
 
-master.rulers().add({'type': "actions", 'group': "notes", 'position': [2, 1], 'lines': [note]})
-master.rulers().add({'type': "actions", 'group': "notes", 'position': [4, 1], 'lines': [note]})
+master.rulers().add({'type': "actions", 'group': "note", 'position': [2, 1], 'lines': [note]})
+master.rulers().add({'type': "actions", 'group': "note", 'position': [4, 1], 'lines': [note]})
 master.rulers().filter(type="actions").sort().print().print_lines()
 
-master.rulers().add({'type': "arguments", 'group': "generic", 'position': [2, 1], 'lines': [None, 'c#', None, None, 'e', None]})
-master.rulers().add({'type': "arguments", 'group': "generic", 'position': [1, 0], 'lines': ['c', 'c#', 'd', None, 'e', None]})
-master.rulers().add({'type': "arguments", 'group': "generic", 'position': [3, 0], 'lines': ['d', 'c#', 'd', 'd#', 'e', None], 'offset': -1})
-master.rulers().add({'type': "arguments", 'group': "specific", 'position': [2, 2], 'lines': [None, 'c#', None, None, 'e', None]})
-master.rulers().add({'type': "arguments", 'group': "specific", 'position': [1, 1], 'lines': ['a', 'b', 'd', None, 'f', None], 'offset': -2})
-master.rulers().add({'type': "arguments", 'group': "specific", 'position': [3, 2], 'lines': [None, 'c#', 'd', 'd#', 'e', None]})
+master.rulers().add({'type': "arguments", 'group': "any_channel", 'position': [0, 0], 'lines': [1]})
+master.rulers().add({'type': "arguments", 'group': "any_velocity", 'position': [0, 0], 'lines': [60]})
+
+master.rulers().add({'type': "arguments", 'group': "key", 'position': [2, 1], 'lines': [None, 'c#', None, None, 'e', None]})
+master.rulers().add({'type': "arguments", 'group': "key", 'position': [1, 0], 'lines': ['c', 'c#', 'd', None, 'e', None]})
+master.rulers().add({'type': "arguments", 'group': "key", 'position': [3, 0], 'lines': ['d', 'c#', 'd', 'd#', 'e', None], 'offset': -1})
+master.rulers().add({'type': "arguments", 'group': "key", 'position': [2, 2], 'lines': [None, 'c#', None, None, 'e', None]})
+master.rulers().add({'type': "arguments", 'group': "key", 'position': [1, 1], 'lines': ['a', 'b', 'd', None, 'f', None], 'offset': -2})
+master.rulers().add({'type': "arguments", 'group': "key", 'position': [3, 2], 'lines': [None, 'c#', 'd', 'd#', 'e', None]})
 master.rulers().filter(type="arguments").sort().print().print_lines(None, 8).spread_lines(-2).print_lines(None, 8).print()
 
 #trigger.play()
