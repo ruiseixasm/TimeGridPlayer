f006ee4fdb3068572a2e41d4f4fc4579f56c44ca
Rui Monteiro <ruiseixas@yahoo.com>
2023-12-12 10:51:11 +0000

    Enumerated drum kit

--- user_drum_patterns_1.py
+++ user_drum_patterns_1.py
@@ -25,11 +25,25 @@ master.rulers().add({'link': "note", 'lines': ["1/4"]}).add({'link': "note", 'li
 master.rulers().copy().move_position([1, 0]).copy().move_position([2, 0]).copy().move_position([3, 0])
 master.rulers().add({'link': "note.key", 'position': [0, 0], 'lines': [60]})
 master.rulers().add({'link': "note.channel", 'position': [0, 0], 'lines': [10]}).print().print_lines(0, 15)
-master.rulers().recall().copy().print().offset_lines(1).print().root().print().lines(1,2).print()
-master.rulers().print().measures(0).print().root().print().beats(1).print().root().print().steps(8).print()
+master.rulers().recall().copy().print().offset_lines(1).print().root().print().lines(1,5).print()
+master.rulers().print().measures(0).print().root().print().beats(1).print().root().print().steps(8, 9, 12, 14).print()
 #master.staff().print()
 
 key_ruler = master.rulers().link_find("key")
+
+drum_kit = {
+    'drums': [33, 35, 36],
+    'hi-hats': [42, 44, 46],
+    'toms': [41, 43, 45, 47, 48, 50],
+    'snares': [38, 40],
+    'cymbals': [49, 51, 52, 55, 57, 59]
+}
+
+for key, values in drum_kit.items():
+    print(key)
+    for value in values:
+        key_ruler.set_lines([value])
+        master.play()
 
 # for note in range(27, 88):
 #     key_ruler.set_lines([note]).print_lines()
